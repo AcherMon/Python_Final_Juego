aee7d8c79522824c0d02b2ea1c4fe135461e777a
Clase <200025@iessanalberto.com>
2025-12-11 13:29:11 +0100

    Hola.2

--- Clases/personajes.py
+++ Clases/personajes.py
@@ -2,25 +2,31 @@ class Personaje:
     def __init__(self, canvas, nombre, x, y):
         self.canvas = canvas
         self.nombre = nombre
-        # Atributos encapsulados (datos del objeto)
         self.x = x
         self.y = y
-        self.color = "#5a636e"  # Color por defecto
+        self.color = "#5a636e"
         self.id_cuerpo = None
         self.id_ojo = None
 
     def dibujar(self):
-        # Dibujo el cuerpo
-        cuerpo_base=10
-        cuerpo_altura=25
+        # dibujar el cuerpo
+        cuerpo_base = 10
+        cuerpo_altura = 25
         x1, y1 = self.x - cuerpo_base, self.y - cuerpo_altura
         x2, y2 = self.x + cuerpo_base, self.y + cuerpo_altura
-
         self.id_cuerpo = self.canvas.create_rectangle(x1, y1, x2, y2, fill=self.color, outline="black")
 
-        # Dibujo el ojo
-        ojo_base=10
-        ojo_altura=5
+        # dibujar el ojo
+        visor_alto = 5
+        ox1 = x1
+        ox2 = x2
+        oy1 = y1 + 8
+        oy2 = oy1 + visor_alto
+        self.id_ojo = self.canvas.create_rectangle(ox1, oy1, ox2, oy2, fill="blue", outline="black")
 
-        ox1, oy1 = self.x - cuerpo_base, self.y - cuerpo_altura
-        ox2, oy2 = self.x + cuerpo_base, self.y + cuerpo_altura
+    def mover(self, dx, dy):
+        # epic movement from Mr.P
+        self.canvas.move(self.id_cuerpo, dx, dy)
+        self.canvas.move(self.id_ojo, dx, dy)
+        self.x += dx
+        self.y += dy
--- ventana.py
+++ ventana.py
@@ -1,15 +1,41 @@
 import tkinter as tk
-import random
-
-
-def obtener_posicion_random(self):
-    x = random.randint(50, 550)
-    y = random.randint(50, 350)
-    return x, y
+from Clases.personajes import Personaje
+from PIL import Image, ImageTk
 
 ventana = tk.Tk()
-ventana.title("El GOTY Of The Year")
+ventana.title("GOTY of the year")
 ventana.geometry("800x700")
-escenario_canvas = (tk.Canvas(ventana, width=800, height=500, bg="#2c3e50"))
+
+canvas_width = 800
+canvas_height = 700
+escenario_canvas = tk.Canvas(label_img, width=canvas_width, height=canvas_height, bg="#2c3e50")
 escenario_canvas.pack(fill=tk.BOTH, expand=True)
+img_fondo = Image.open("assets/suelo.jpg")
+imagen_resize = img_fondo.resize((800,700),Image.LANCZOS)
+imgtk = ImageTk.PhotoImage(imagen_resize)
+label_img = tk.Label(ventana, image=imgtk)
+label_img.pack(padx=10, pady=10)
+
+# Crear Mini PEKKA en el centro
+x_centro = canvas_width // 2
+y_centro = canvas_height // 2
+mini_pekka = Personaje(escenario_canvas, "Mini PEKKA", x_centro, y_centro)
+mini_pekka.dibujar()
+
+# Movimiento
+def mover_izquierda(event):
+    mini_pekka.mover(-10, 0)
+def mover_derecha(event):
+    mini_pekka.mover(10, 0)
+def mover_arriba(event):
+    mini_pekka.mover(0, -10)
+def mover_abajo(event):
+    mini_pekka.mover(0, 10)
+
+# Vincular teclas para el movimiento
+ventana.bind("<Left>", mover_izquierda)
+ventana.bind("<Right>", mover_derecha)
+ventana.bind("<Up>", mover_arriba)
+ventana.bind("<Down>", mover_abajo)
+
 ventana.mainloop()
